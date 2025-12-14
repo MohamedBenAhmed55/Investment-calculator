fe55d33f537a719d1e19db0fe13cccb923908c00
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-12-14 21:42:33 +0100

    Cross-Component Communication with Outputs

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -6,20 +6,27 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
 
   enteredInitialInvestment: string = '0';
   enteredAnnualInvestment: string = '0';
   enteredExpectedReturn: string = '5';
   enteredDuration: string = '10';
 
-  onSubmit(){
-    console.log('Form submitted!');
-    console.log('Initial Investment:', this.enteredInitialInvestment);
-    console.log('Annual Investment:', this.enteredAnnualInvestment);
-    console.log('Expected Return:', this.enteredExpectedReturn);
-    console.log('Duration:', this.enteredDuration);
+  onSubmit() {
+    this.calculate.emit({
+    initialInvestment: +this.enteredInitialInvestment,
+    duration: +this.enteredDuration,
+    expectedReturn: +this.enteredExpectedReturn,
+    annualInvestment: +this.enteredAnnualInvestment
+  })
   }
 }
