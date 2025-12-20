e529ca6735ef67bac2323290032e210058587f44
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-12-21 00:08:17 +0100

    Using Signals & Resetting The Form After Submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvetmentInput } from './invetment-input.model';
@@ -12,14 +12,24 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
 })
 export class AppComponent {
 
-  resultsData?: {
+  // resultsData?: {
+  //     year: number,
+  //     interest: number,
+  //     valueEndOfYear: number,
+  //     annualInvestment: number,
+  //     totalInterest: number,
+  //     totalAmountInvested: number,
+  //   }[]
+
+  resultsData = signal<{
       year: number,
       interest: number,
       valueEndOfYear: number,
       annualInvestment: number,
       totalInterest: number,
       totalAmountInvested: number,
-    }[]
+    }[] | undefined>(undefined)
+
   onCalculateInvestmentResults(
     data: InvetmentInput
   ) {
@@ -44,6 +54,6 @@ export class AppComponent {
     }
 
     // return annualData;
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, signal, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type  {InvetmentInput } from '../invetment-input.model';
 
@@ -10,19 +10,30 @@ import type  {InvetmentInput } from '../invetment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InvetmentInput>();
+  // @Output() calculate = new EventEmitter<InvetmentInput>();
+  calculate = output<InvetmentInput>();
 
-  enteredInitialInvestment: string = '0';
-  enteredAnnualInvestment: string = '0';
-  enteredExpectedReturn: string = '5';
-  enteredDuration: string = '10';
+  // enteredInitialInvestment: string = '0';
+  // enteredAnnualInvestment: string = '0';
+  // enteredExpectedReturn: string = '5';
+  // enteredDuration: string = '10';
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
   onSubmit() {
     this.calculate.emit({
-    initialInvestment: +this.enteredInitialInvestment,
-    duration: +this.enteredDuration,
-    expectedReturn: +this.enteredExpectedReturn,
-    annualInvestment: +this.enteredAnnualInvestment
+    initialInvestment: +this.enteredInitialInvestment(),
+    duration: +this.enteredDuration(),
+    expectedReturn: +this.enteredExpectedReturn(),
+    annualInvestment: +this.enteredAnnualInvestment()
   })
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
